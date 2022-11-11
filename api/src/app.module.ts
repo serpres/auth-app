4e89c365dc3381cd58d5b3f8bdcb5d5ca3873564
Sergey Presnyakov <serpres77@gmail.com>
2022-11-11 08:32:00 +0300

    backend: add orm

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import ormconfig from '@app/ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.local', '.env.development'],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(ormconfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
