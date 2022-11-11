f3420e6fa19e7d33e0f05d55e35f96e0a69e2b98
Sergey Presnyakov <serpres77@gmail.com>
2022-11-11 07:07:37 +0300

    backend: add aliases

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,6 @@
+require('module-alias/register');
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
